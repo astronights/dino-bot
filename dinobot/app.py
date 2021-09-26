185369d4b71d6c5529c8fee0de8bc1df3b6f258f
astronights <shubhankar.a31@gmail.com>
2021-09-26 22:41:58 +0800

    Attempts to fix offline mode

--- dinobot/app.py
+++ dinobot/app.py
@@ -3,5 +3,4 @@ from game.control import GameControl
 def run():
     game = GameControl()
     game.load_game()
-    # game.start_game()
-    print("Hello World")
+    game.start_game()
--- dinobot/game/control.py
+++ dinobot/game/control.py
@@ -5,18 +5,20 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 class GameControl():
     def __init__(self):
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        options.add_experimental_option("detach", True)
         self.DINO_URL="chrome://dino"
-        self.driver = webdriver.Chrome()
-        # self.actionChains = ActionChains(self.driver)
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.set_network_conditions(offline=True, latency=5, throughput=500 * 1024)
+        self.actionChains = ActionChains(self.driver)
 
     def load_game(self):
-        try:
-            self.driver.get(self.DINO_URL)
-        except Exception:
-            pass
+        self.driver.get(self.DINO_URL)
 
     def close_game(self):
         self.driver.close()
 
-    # def start_game(self):
-        # self.actionChains.send_keys(Keys.SPACE).perform()
+    def start_game(self):
+        print("Starting game...")
+        self.actionChains.send_keys(Keys.SPACE).perform()
