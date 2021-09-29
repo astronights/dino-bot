832bcbc219188ea2ea33952d6ecb9937b656360a
astronights <shubhankar.a31@gmail.com>
2021-09-29 22:47:24 +0800

    Try to bring browser to front

--- dinobot/game/control.py
+++ dinobot/game/control.py
@@ -8,13 +8,17 @@ class GameControl():
         options = webdriver.ChromeOptions()
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         options.add_experimental_option("detach", True)
+        options.add_argument("--start-maximized")
         self.DINO_URL="http://127.0.0.1:5000"
         self.driver = webdriver.Chrome(options=options)
+        self.driver.maximize_window()
+        print(self.driver.window_handles)
         # self.driver.set_network_conditions(offline=True, latency=5, throughput=500 * 1024)
         self.actionChains = ActionChains(self.driver)
 
     def load_game(self):
         self.driver.get(self.DINO_URL)
+        self.driver.switch_to.window(self.driver.window_handles[-1])
 
     def close_game(self):
         self.driver.close()
