b9f676141ac88fd5ad5b21522e513a55eefd55c6
astronights <shubhankar.a31@gmail.com>
2021-10-05 22:00:50 +0800

    Basic game starts

--- dinobot/game/control.py
+++ dinobot/game/control.py
@@ -8,20 +8,23 @@ class GameControl():
         options = webdriver.ChromeOptions()
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         options.add_experimental_option("detach", True)
-        options.add_argument("--start-maximized")
+        ## Maximizing window leads to actions not working
         self.DINO_URL="http://127.0.0.1:5000"
         self.driver = webdriver.Chrome(options=options)
-        self.driver.maximize_window()
         self.actionChains = ActionChains(self.driver)
 
     def load_game(self):
         self.driver.get(self.DINO_URL)
-        self.driver.switch_to.window(self.driver.window_handles[-1])
+        print(self.driver.get_window_position())
+        print(self.driver.get_window_rect())
+        print(self.driver.get_window_size())
+        # self.driver.set_window_size()
+        # self.driver.switch_to.window(self.driver.window_handles[-1])
 
     def close_game(self):
+        print("Closing game...")
         self.driver.close()
 
     def start_game(self):
         print("Starting game...")
-        print(self.driver.window_handles)
         self.actionChains.click(self.driver.find_element_by_id("dino-frame")).send_keys(Keys.SPACE).perform()
