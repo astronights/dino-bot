44c623ffc74a0617c51d0ea37649ddaedd5962c9
astronights <shubhankar.a31@gmail.com>
2021-10-05 23:10:56 +0800

    Located game canvas

--- dinobot/game/control.py
+++ dinobot/game/control.py
@@ -2,6 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import pyautogui
 
 class GameControl():
@@ -9,18 +13,20 @@ class GameControl():
         options = webdriver.ChromeOptions()
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         options.add_experimental_option("detach", True)
-        ## Maximizing window leads to actions not working
         self.DINO_URL="http://127.0.0.1:5000"
         self.driver = webdriver.Chrome(options=options)
         self.actionChains = ActionChains(self.driver)
         self.screen = pyautogui.size()
-        self.pos = None
 
     def load_game(self):
         self.driver.get(self.DINO_URL)
         self.pos = self.driver.get_window_rect()
         self.driver.minimize_window()
         self.driver.maximize_window()
+        WebDriverWait(self.driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.ID,"dino-frame")))
+        self.dinoCanvas = self.driver.find_elements_by_class_name("runner-canvas")[0]
+        self.canvasPos = self.dinoCanvas.location
+        self.canvasSize = self.dinoCanvas.size
 
     def close_game(self):
         print("Closing game...")
@@ -28,8 +34,7 @@ class GameControl():
 
     def start_game(self):
         print("Starting game...")
-        frameElem = self.driver.find_element_by_id("dino-frame")
-        self.actionChains.move_to_element_with_offset(frameElem, 10, 10)
+        self.actionChains.move_to_element(self.dinoCanvas)
         self.actionChains.click()
         self.actionChains.send_keys(Keys.SPACE)
         self.actionChains.perform()
